9929c656f0342278e34ba2b3f05b91642f59afe5
SamuraiNeo <corpo369@onet.pl>
2017-05-13 19:39:06 +0200

    Create spammer.py

---
+++ spammer.py
@@ -0,0 +1,19 @@
+import smtplib
+
+fromaddress = "fromaddress@gmail.com"
+toaddress = "toaddress@gmail.com"
+username = "yourgmail username"
+password = "yourgmail password"
+message = """From:CodeScripter 
+...text here...
+"""
+
+server =smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+
+for i in range (0,10):
+  server.sendmail(fromaddress,toaddress,message)
+  print("Mail sent")
+
+server.quit()
